359115e6c4f65dcf780986867699666e72b88e8c
Mohit Singh <mosingh8878@gmail.com>
2025-04-08 23:56:45 +0530

    Update cc.cpp

--- cc.cpp
+++ cc.cpp
@@ -6,5 +6,6 @@ int main()
     std::cout << "This is a C++ program." << std::endl;
     std::cout << "It prints a message to the console." << std::endl;
     std::cout << "Goodbye!" << std::endl;
+    std:cout << "Web side changes" << std::endl;
     return 0;
 }
